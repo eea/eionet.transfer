a60b6ac979f587267a3271b24e27fce9bab0202f
Søren Roug <soren.roug@eea.europa.eu>
2015-08-16 12:55:51 +0200

    More tests

--- src/test/java/eionet/transfer/util/HumaneTest.java
+++ src/test/java/eionet/transfer/util/HumaneTest.java
@@ -6,12 +6,27 @@ import static org.junit.Assert.assertEquals;
 public class HumaneTest {
 
     @Test
+    public void nosize() {
+        assertEquals("0", Humane.humaneSize(0L));
+    }
+
+    @Test
+    public void singleDigit() {
+        assertEquals("9", Humane.humaneSize(9L));
+    }
+
+    @Test
     public void underThousand() {
         assertEquals("145", Humane.humaneSize(145L));
     }
 
     @Test
     public void kilo() {
+        assertEquals("1.23 KB", Humane.humaneSize(1234L));
+    }
+
+    @Test
+    public void kiloRoundUp() {
         assertEquals("1.46 KB", Humane.humaneSize(1456L));
     }
 
@@ -28,6 +43,21 @@ public class HumaneTest {
     @Test
     public void mega() {
         assertEquals("1.46 MB", Humane.humaneSize(1456000L));
+    }
+
+    @Test
+    public void mega10() {
+        assertEquals("14.6 MB", Humane.humaneSize(14560000L));
+    }
+
+    @Test
+    public void giga() {
+        assertEquals("1.46 GB", Humane.humaneSize(1456000000L));
+    }
+
+    @Test
+    public void giga100() {
+        assertEquals("146 GB", Humane.humaneSize(145600000000L));
     }
 
 }
