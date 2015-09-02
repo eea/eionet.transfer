a4703fba05df303cbf2ac31d1b22060b7913874a
Søren Roug <soren.roug@eea.europa.eu>
2015-09-02 19:01:02 +0200

    Allow users with no roles to log in.

--- src/main/java/eionet/transfer/dao/UserManagementServiceJdbc.java
+++ src/main/java/eionet/transfer/dao/UserManagementServiceJdbc.java
@@ -22,21 +22,41 @@ package eionet.transfer.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.GroupManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 
+
 /**
- * Extends Spring's JDBC implementation to add a list of all users.
+ * Extends Spring's JDBC implementation to add a list of all users and accept users thart are not in the database.
  */
 public class UserManagementServiceJdbc extends JdbcUserDetailsManager
         implements UserManagementService, UserDetailsManager, GroupManager {
 
     private static final String DEF_GET_ALL_USERS_SQL = "SELECT * FROM users";
+
+    /** The users table has only the people with roles, so we fake the check as we're using CAS. */
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        Set<GrantedAuthority> dbAuthsSet = new HashSet<GrantedAuthority>();
+
+        if (getEnableAuthorities()) {
+            dbAuthsSet.addAll(loadUserAuthorities(username));
+        }
+
+        List<GrantedAuthority> dbAuths = new ArrayList<GrantedAuthority>(dbAuthsSet);
+        return new User(username, "", true, true, true, true, dbAuths);
+    }
 
     @Override
     public List<UserDetails> getAllUsers() {
